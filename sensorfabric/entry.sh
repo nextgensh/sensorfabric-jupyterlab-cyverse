a83ac292bec2116f712d2fd86d2fcaae30e42f01
Shravan Aras <123.shravan@gmail.com>
2023-05-09 22:49:06 -0700

    Moved AWS creds to env variables.
    1. Fixed working directory for Cyverse.

--- sensorfabric/entry.sh
+++ sensorfabric/entry.sh
@@ -2,12 +2,6 @@
 
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 
-# Write the environment variables into the .aws files.
-FILE=/home/jovyan/.aws/credentials
-echo "[uoa]" >> $FILE
-echo "aws_access_key_id=$AWS_ACCESS_KEY" >> $FILE
-echo "aws_secret_access_key=$AWS_SECRET_KEY" >> $FILE
-echo "region=us-east-1" >> $FILE
-export AWS_PROFILE="uoa"
+export AWS_DEFAULT_REGION=us-east-1
 
 exec jupyter lab --no-browser --LabApp.token="" --LabApp.password=""
